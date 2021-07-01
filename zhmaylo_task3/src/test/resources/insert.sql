9f56eacc797483736620ab2a1fa861d5807f1394
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-07-01 17:46:28 +0700

    Delete zhmaylo_task3/src/test directory
    do tests for task3

--- zhmaylo_task3/src/test/resources/insert.sql
+++
@@ -1,3 +0,0 @@
-insert into message(id, message, show) values (1, 'Oohan', true);
-insert into message(id, message, show) values (2, 'Twooh', true);
-insert into message(id, message, show) values (3, 'Treooh', true);
--- zhmaylo_task3/src/test/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE message (
-                         id INT NOT NULL,
-                         message VARCHAR(40000) NOT NULL,
-                         show boolean,
-                         PRIMARY KEY(id)
-);
